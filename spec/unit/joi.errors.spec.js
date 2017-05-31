d77e08d6f37abae11c976385e2bdaed4f8ca62fd
Weber Amaral <weberamaral@outlook.com>
2017-05-31 18:12:31 -0300

    Adicionado spec first para joi-errors

---
+++ spec/unit/joi.errors.spec.js
@@ -0,0 +1,33 @@
+/**
+ * @licence
+ * Copyright 2016 Monte sua Viagem. All Rights Reserved.
+ */
+
+describe('# JoiErrors', () => {
+  it('Should exists', (done) => {
+    done();
+  });
+  describe('## parse()', () => {
+    it('Should exists', (done) => {
+      done();
+    });
+    it('Should be an function', (done) => {
+      done();
+    });
+    it('Should required ne argument', (done) => {
+      done();
+    });
+    it('Should return an array', (done) => {
+      done();
+    });
+    it('Should transform an JoiError to custom error', (done) => {
+      done();
+    });
+    it('Should transform an JoiError to custom error with property message', (done) => {
+      done();
+    });
+    it('Should transform an JoiError to custom error with property attribute', (done) => {
+      done();
+    });
+  });
+});
